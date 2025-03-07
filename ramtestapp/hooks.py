fdc9aa532a7194b08abe3473a8139bc4723e69b4
Ubuntu <ubuntu@ip-172-31-20-109.ec2.internal>
2025-03-07 06:34:14 +0000

    Updated the hooks.py file

--- ramtestapp/hooks.py
+++ ramtestapp/hooks.py
@@ -5,64 +5,64 @@ app_description = "test description"
 app_email = "ram@walue.biz"
 app_license = "mit"
 
-# Apps
-# ------------------
+ Apps
+ ------------------
 
-# required_apps = []
+required_apps = []
 
-# Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "ramtestapp",
-# 		"logo": "/assets/ramtestapp/logo.png",
-# 		"title": "Ramtestapp",
-# 		"route": "/ramtestapp",
-# 		"has_permission": "ramtestapp.api.permission.has_app_permission"
-# 	}
-# ]
+ Each item in the list will be shown as an app in the apps page
+ add_to_apps_screen = [
+ 	{
+ 		"name": "ramtestapp",
+ 		"logo": "/assets/ramtestapp/logo.png",
+ 		"title": "Ramtestapp",
+ 		"route": "/ramtestapp",
+ 		"has_permission": "ramtestapp.api.permission.has_app_permission"
+ 	}
+ ]
 
-# Includes in <head>
-# ------------------
+ Includes in <head>
+ ------------------
 
-# include js, css files in header of desk.html
-# app_include_css = "/assets/ramtestapp/css/ramtestapp.css"
-# app_include_js = "/assets/ramtestapp/js/ramtestapp.js"
+ include js, css files in header of desk.html
+ app_include_css = "/assets/ramtestapp/css/ramtestapp.css"
+ app_include_js = "/assets/ramtestapp/js/ramtestapp.js"
 
-# include js, css files in header of web template
-# web_include_css = "/assets/ramtestapp/css/ramtestapp.css"
-# web_include_js = "/assets/ramtestapp/js/ramtestapp.js"
+ include js, css files in header of web template
+ web_include_css = "/assets/ramtestapp/css/ramtestapp.css"
+ web_include_js = "/assets/ramtestapp/js/ramtestapp.js"
 
-# include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "ramtestapp/public/scss/website"
+ include custom scss in every website theme (without file extension ".scss")
+ website_theme_scss = "ramtestapp/public/scss/website"
 
-# include js, css files in header of web form
-# webform_include_js = {"doctype": "public/js/doctype.js"}
-# webform_include_css = {"doctype": "public/css/doctype.css"}
+ include js, css files in header of web form
+ webform_include_js = {"doctype": "public/js/doctype.js"}
+ webform_include_css = {"doctype": "public/css/doctype.css"}
 
-# include js in page
-# page_js = {"page" : "public/js/file.js"}
+ include js in page
+ page_js = {"page" : "public/js/file.js"}
 
-# include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+ include js in doctype views
+ doctype_js = {"doctype" : "public/js/doctype.js"}
+ doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+ doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+ doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
-# Svg Icons
-# ------------------
-# include app icons in desk
-# app_include_icons = "ramtestapp/public/icons.svg"
+ Svg Icons
+ ------------------
+ include app icons in desk
+ app_include_icons = "ramtestapp/public/icons.svg"
 
-# Home Pages
-# ----------
+ Home Pages
+ ----------
 
-# application home page (will override Website Settings)
-# home_page = "login"
+ application home page (will override Website Settings)
+ home_page = "login"
 
-# website user home page (by Role)
-# role_home_page = {
-# 	"Role": "home_page"
-# }
+ website user home page (by Role)
+ role_home_page = {
+ 	"Role": "home_page"
+ }
 
 # Generators
 # ----------
