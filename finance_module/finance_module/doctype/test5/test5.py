d986f66229870e7d3e0485a855a80545a0997bb7
Ubuntu <ubuntu@ip-172-31-20-36.ec2.internal>
2025-03-13 05:59:29 +0000

    Intial push

---
+++ finance_module/finance_module/doctype/test5/test5.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Shilparani and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class test5(Document):
+	pass
